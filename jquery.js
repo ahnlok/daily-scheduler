a82b555214ba7c7b9b4ba8fd9074096e1ace245c
Sungpil An <ansungpil1@gmail.com>
2020-12-16 17:54:45 -0500

    Added localStorage function and button to jquery.js

--- jquery.js
+++ jquery.js
@@ -1,23 +1,47 @@
-$(document).ready(function() {   //moment.js variable
+$(document).ready(function() {   
+    //moment.js variable
     var timeDate = moment().format('MMMM Do YYYY, h:mm:ss a');
     $("#currentDay").append(timeDate);
     
     //Declared variable
-    var arrayOfHours = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
-    timeDetector();
+    var arrayOfHours = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+    var currentTime = moment();
 
     function timeDetector() {
-        //current time variable declared
-        var currentZone = moment().format('H');
-        //for loop 
+        console.log("function is working");
+        //current time variable declare
+        var currentZone = moment().format('HH'); 
+         console.log(currentZone)
         for(var i = 0; i < arrayOfHours.length; i++) {
             if(parseInt(arrayOfHours[i]) < currentZone) {
-                $("#" + arrayOfHours[i]).attr("style", "background-color: gray");
+                $("#row" + arrayOfHours[i]).addClass("past");
             }
-            
         }
+    timeDetector();
     }
+    //localStorage to store saved placeholder 
+    function saveMemo () {
+       $(".row").each(function () {
+           var id = $(this).attr("id");
+           var schedule = localStorage.getItem(id);
+
+           if (schedule !== null) {
+               $(this).children("input").val(schedule);
+           }
+       });
+    }
+    saveMemo ();
+    //on click event to send input value into localStorage
+    $(".button").on("click", function() {
+        var timeSave = $(this).parent().attr("id");
+        var memoSave = $(this).siblings("input").val();
+
+        localStorage.setItem(timeSave, memoSave);
+        // console.log(arrayOfHours, memo);
+    });
+    
 });
+
 // Current day is displayed at the top of the calendar
 
 //Time blocks with hours
